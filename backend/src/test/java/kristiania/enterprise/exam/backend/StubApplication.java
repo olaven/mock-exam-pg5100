2b10838c82bd8240e00372279ead3c3bc164dde0
olaven <olavsundfoer@gmail.com>
2019-05-04 16:47:16 +0200

    jacoco restructure

--- backend/src/test/java/kristiania/enterprise/exam/backend/StubApplication.java
+++ backend/src/test/java/kristiania/enterprise/exam/backend/StubApplication.java
@@ -2,10 +2,7 @@ package kristiania.enterprise.exam.backend;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableAsync // IMPORTANT: otherwise @Async will not work
 @SpringBootApplication
--- frontend/src/main/java/kristiania/enterprise/exam/Application.java
+++ frontend/src/main/java/kristiania/enterprise/exam/Application.java
@@ -1,5 +1,6 @@
 package kristiania.enterprise.exam;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
